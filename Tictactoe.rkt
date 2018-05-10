2f250d89027c3db881b8d2cf9343d71f2bad3e63
Joseph Vargas <joseph@cpokemon.com>
2018-05-09 22:02:28 -0600

    Inicio diagonales

--- Tictactoe.rkt
+++ Tictactoe.rkt
@@ -1,9 +1,30 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname Tictactoe) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-
 ;;Importar Interfaz
 (require "interface.rkt")
+
+(define test '((1 2 3)
+               (4 5 6)
+               (7 8 9)))
+
+
+(define testh '((1 2 3 4 5)
+               (6 7 8 9)
+               (10 11 12 13)))
+
+(define testv '((1 2 3)
+               (4 5 6)
+               (7 8 9)
+               (10 11 12)
+               (13 14 15)))
+
+
+(define testxy '((1 2 3 4 5)
+                   (6 7 8 9 10)
+                   (11 12 13 14 15)
+                   (16 17 18 19 20)))
+
 ;; Funcion que Detecta Ganador
 ;; Matriz de ejemlo (( x x x )( x x x )( x x x ))
 ;; (check_Winner  '(( x x x )( x x x )(x x x))   )
@@ -55,4 +76,49 @@
    
     (else (append (list(car(car matrix))) (traspuesta_aux (cdr matrix) )))))
 
-;; Genera las diagonales de la matriz
+;Funcion que obtiene un valor en un indice i j de una matriz
+(define (getAtIndex matrix i j)
+  (list-ref (list-ref matrix i) j)
+  )
+
+(define (inverseMatrix matrix)
+  (cond
+    ((empty? matrix) '())
+    (else (cons (reverse (car matrix)) (inverseMatrix (cdr matrix))))
+    )
+  )
+
+
+(define (getInvDiagonals matrix)
+  (getDiagonals (inverseMatrix matrix))
+  )
+
+
+(define (getDiagonals matrix)
+  (cond
+    ((< (length matrix) (length (car matrix))) (getDiagonalsIrregularH matrix 0 (length (car matrix)) (length matrix)))
+    ((> (length matrix) (length (car matrix))) (getDiagonalsIrregularV matrix 0 (length (car matrix)) (length matrix)))
+   ((equal? (length matrix) (length (car matrix))) (getDiagonalsRegular matrix (length matrix)))
+   )
+  )
+
+(define (getDiagonalsRegular matrix len)
+  (cond
+    ((zero? len) '())
+    (else (cons (getAtIndex matrix (- len 1) (- len 1)) (getDiagonalsRegular matrix (- len 1))))
+    )
+  )
+
+(define (getDiagonalsIrregularH matrix index lenx leny)
+  (cond
+    ((< lenx (+ index leny)) '())
+    (else (cons (getDiagonalsRegular matrix leny) (getDiagonalsIrregularH (corta_matrix matrix) (+ index 1) lenx leny)))
+    )
+  )
+
+(define (getDiagonalsIrregularV matrix index lenx leny)
+  (cond
+    ((< leny (+ index lenx)) '())
+    (else (cons (getDiagonalsRegular matrix lenx) (getDiagonalsIrregularV (cdr matrix) (+ index 1) lenx leny)))
+    )
+  )
